80a2ac010a3b1fa94b2c3a9202f683c590614a2f
Paul Maloka <59463155@mylife.unisa.ac.za>
2023-02-17 13:18:42 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; 'a' <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
