7c8c906bc1425bc1d35fb71fa7cb3635712c90dd
Paul Maloka <59463155@mylife.unisa.ac.za>
2023-02-17 16:00:50 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+        char i;
+
+        for (i = 'a' ; i <= 'z'; i++)
+        {
+		putchar(i);
+        }
+	for (i = 'a' ; i <= 'z'; i++)
+        {
+                putchar(toupper(i));
+        }
+                putchar('\n');
+        return (0);
+}
